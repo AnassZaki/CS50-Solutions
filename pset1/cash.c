c79821c81e343dbdfaeb1b91c0f30b2fda5b18d3
Anass Zaki <thezaki.anass@gmail.com>
2019-02-01 12:11:40 +0100

    Solved CS50's Cash Problem.

--- pset1/cash.c
+++ pset1/cash.c
@@ -1,13 +1,21 @@
-#include <stdio.h>
 #include <cs50.h>
+#include <stdio.h>
+#include <math.h>
 
 int main(void)
 {
-    int coins;
+    float dollar = 0;
     do
     {
-        coins = get_int("Amount: \n")
+        dollar = get_float("Amount: ");
+    } while (dollar < 0);
+    int coins = round(dollar * 100);
+    int divisor[4] = {25, 10, 5, 1};
+    int count = 0;
+    for (int i = 0; i < 4; i++)
+    {
+        count += coins / divisor[i];
+        coins = coins % divisor[i];
     }
-    while(coins < 0);
-    
+    printf("%i\n", count);
 }
